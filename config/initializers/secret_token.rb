9526aac882d36108dde560c5c654016407e2a732
Dirk Kelly <git+dk@dirkkelly.com>
2012-08-21 19:02:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swansonipsum::Application.config.secret_token = '00074783c9538e13072f4a360c0298109867e902f8d8226445eccc3ba3de9124ddbfc5f1f925e835fc9b2ae61650cbe6dac9dd333c583e9854563e475dbe50cd'
