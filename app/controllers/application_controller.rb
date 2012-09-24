421117739496936535fd0af80fca9bdead69b048
Dirk Kelly <git+dk@dirkkelly.com>
2012-09-24 01:08:28 -0400

    Very basic pjax implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :set_layout
+
+  private
+
+  def set_layout
+    if request.headers['X-PJAX']
+      false
+    else
+      "application"
+    end
+  end
 end
