a086e3764e824f84dab015addfed57c39e26b2ab
Dirk Kelly <git+dk@dirkkelly.com>
2012-09-29 16:39:38 -0400

    Remove global navigation Show current navigation highlighted Improve acceptance specs

--- spec/acceptance/ipsums_spec.rb
+++ spec/acceptance/ipsums_spec.rb
@@ -1,29 +1,32 @@
 require 'spec_helper'
 
 feature "Ipsums" do
-  it "shows ipsums when the url is hit directly" do
+  background do
     visit ipsums_path
+  end
 
-    within "#ipsums" do
-      page.text.split(" ").length.should <= 90
-      page.all('p').length.should == 3
-      page.find('p:first-of-type').text.split(" ").length.should <= 30
+  describe "shows ipsums when the url is hit directly" do
+    it { page.find('#ipsums').text.split(" ").length.should <= 90 }
+    it { page.find('#ipsums p:first-of-type').text.split(" ").length.should <= 30 }
+    it { page.all('#ipsums p').length.should == 3 }
+    it { page.find('.nav-tabs li.active').text.should == "Ipsums" }
+  end
+
+  describe "generates new ipsums on click of the link" do
+    before do
+      @original_ipsums_length = page.find('#ipsums').text.length
+      click_link "Ipsums"
     end
+
+    it { page.find('#ipsums').text.length.should_not == @original_ipsums_length }
   end
 
-  it "generates new ipsums on click of the link" do
-    visit ipsums_path
+  describe "takes you to the ipsums url on click of the link", js: true do
+    before do
+      click_link "Ipsums"
+    end
 
-    original_ipsums_length = page.find('#ipsums').text.length
-
-    click_link "Ipsums"
-    page.find('#ipsums').text.length.should_not == original_ipsums_length
-  end
-
-  it "takes you to the ipsums url on click of the link", js: true do
-    visit ipsums_path
-
-    click_link "Ipsums"
-    page.current_path.should == "/ipsums"
+    it { page.current_path.should == "/ipsums" }
+    it { page.find('.nav-tabs li.active').text.should == "Ipsums" }
   end
 end
--- spec/acceptance/quotes_spec.rb
+++ spec/acceptance/quotes_spec.rb
@@ -1,34 +1,30 @@
 require 'spec_helper'
 
 feature "Quotes" do
-  it "shows quotes on load" do
-    visit root_path
-
-    page.should have_css('#quotes')
+  background do
+    visit quotes_path
   end
 
-  it "shows quotes when the url is hit directly" do
-    visit quotes_path
+  describe "shows quotes when the url is hit directly" do
+    it { page.all('#quotes p').length.should >= 3 }
+    it { page.find('.nav-tabs li.active').text.should == "Quotes" }
+  end
 
-    within "#quotes" do
-      page.text.split(" ").length.should > 50
-      page.all('p').length.should == 3
+  describe "generates new quotes on click of the link" do
+    before do
+      @original_quotes_length = page.find('#quotes').text.length
+      click_link "Quotes"
     end
+
+    it { page.find('#quotes').text.length.should_not == @original_quotes_length }
   end
 
-  it "generates new quotes on click of the link" do
-    visit quotes_path
-
-    original_quotes_length = page.find('#quotes').text.length
-
-    click_link "Quotes"
-    page.find('#quotes').text.length.should_not == original_quotes_length
-  end
-
-  it "takes you to the quotes url on click of the link", js: true do
-    visit quotes_path
-
-    click_link "Quotes"
-    page.current_path.should == "/quotes"
+  describe "takes you to the quotes url on click of the link", js: true do
+    before do
+      click_link "Quotes"
+    end
+    
+    it { page.current_path.should == "/quotes" }
+    it { page.find('.nav-tabs li.active').text.should == "Quotes" }
   end
 end
